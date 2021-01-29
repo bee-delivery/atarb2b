2b81b74552dd7950dd6f974613be8c59dafebd3f
flads <flads.me@gmail.com>
2021-01-29 11:51:04 -0300

    get account

--- src/Models/Account.php
+++ src/Models/Account.php
@@ -6,5 +6,23 @@ use BeeDelivery\AtarB2B\Utils\Connection;
 
 class Account
 {
+    public $http;
+    protected $account;
 
+    public function __construct()
+    {
+        $this->http = new Connection();
+    }
+
+    /**
+     * Retrieves an account.
+     *
+     * @see https://baas-dot-wearatar-dev.appspot.com/docs/origination
+     * @param String $atarId
+     * @return Array
+     */
+    public function getAccount($atarId)
+    {
+        return $this->http->get('/accounts/' . $atarId);
+    }
 }
