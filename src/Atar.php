db8631cc2f2c2c886b10d87889fa5ef472ac38b8
Luan <luan@luanrodrigues.com.br>
2021-03-19 16:11:23 -0300

    ajuste

--- src/Atar.php
+++ src/Atar.php
@@ -6,7 +6,7 @@ use BeeDelivery\AtarB2B\Pix;
 
 class Atar
 {
-    public function pix($baas = null, $headers = null) {
+    public function pix($baas = null, $headers = array()) {
         return new Pix($baas, $headers);
     }
 }
